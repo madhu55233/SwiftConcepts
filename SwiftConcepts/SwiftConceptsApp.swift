68f8fa5cd361e407a7da3ce0bb84ebb26d700f1a
Madhumitha <madhumithanagaraj0523@gmail.com>
2026-06-08 14:44:34 +0530

    Initial Commit

---
+++ SwiftConcepts/SwiftConceptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConceptsApp.swift
+//  SwiftConcepts
+//
+//  Created by Madhumitha on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConceptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
